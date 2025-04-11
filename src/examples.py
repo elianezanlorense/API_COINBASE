1f0c213a1d9809735514519746b8f6cee286325a
“ezanlorense” <zanlorense.eli@example.com>
2025-04-11 13:19:19 +0900

    Using json

--- src/examples.py
+++ src/examples.py
@@ -1,9 +1,16 @@
 import requests
 
 
-url='https://www.infomoney.com.br'
-resposta=requests.get(url)
+#url='https://www.infomoney.com.br'
+#resposta=requests.get(url)
 #print(url)
 #print(resposta)
 #print(resposta.text)
 #print(resposta.headers)
+
+
+#using json
+url='https://jsonplaceholder.typicode.com/posts/1'
+response=requests.get(url)
+data =response.json()
+print(data)
